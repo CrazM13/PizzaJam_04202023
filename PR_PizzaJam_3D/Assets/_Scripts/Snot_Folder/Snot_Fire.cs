0ba62d0d9f7978413d344cf329045ef9a56f7f46
Rui Santos <cardkingrui@gmail.com>
2023-04-22 11:20:27 -0400

    Fixed Snot
    Fixed snot to use Time.time and to be reflected.

--- PR_PizzaJam_3D/Assets/_Scripts/Snot_Folder/Snot_Fire.cs
+++ PR_PizzaJam_3D/Assets/_Scripts/Snot_Folder/Snot_Fire.cs
@@ -9,25 +9,37 @@ public class Snot_Fire : MonoBehaviour
     private Transform startingPosition;
     public float timeLerpStart;
     public float lerpTime = 1;
+    public bool isReflected;
 
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("target").transform;
         startingPosition = this.transform;
-        timeLerpStart = Time.deltaTime;
+        timeLerpStart = Time.time;
+        isReflected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position= snotFired(startingPosition.position, target.position, timeLerpStart, lerpTime);
+            this.transform.position = snotFired(startingPosition.position, target.position, timeLerpStart, lerpTime);
     }
 
     public Vector3 snotFired(Vector3 start, Vector3 end, float timeLerping, float lerpTiming)
     {
-        float TimePassed = (Time.deltaTime - timeLerping)*3;
+        float TimePassed = Time.time - timeLerping;
         float percentageLerped = TimePassed / lerpTiming;
         var result = Vector3.Lerp(start, end, percentageLerped);
         return result;
+    }
+
+    public void reflected()
+    {
+        if (isReflected == false)
+        {
+            isReflected = true;
+            target.position = startingPosition.position;
+            startingPosition = this.transform;
+        }
     }
 }
